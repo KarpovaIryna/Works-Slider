f8d6b9faa2257aa6964f682e60bec4e60c838154
KarpovaIryna <karpovai@outllok.com>
2020-06-17 16:56:25 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 "use strict";
- const slider = $('.ba-slick');
- slider.slick({
+const slider = $('.ba-slick');
+slider.slick({
 	infinite: true,
-	slide:'.ba-slick__slide',
+	slide: '.ba-slick__slide',
 	slidesToShow: 3,
 	slidesToScroll: 1,
 	prevArrow: '.ba-slick__prev',
 	nextArrow: '.ba-slick__next',
 	variableWidth: true,
-	adaptiveHeight:true,
+	adaptiveHeight: true,
 	responsive: [
 		{
 			breakpoint: 1640,
 			settings: {
-			  slidesToShow: 2,
-			  slidesToScroll: 1,
+				slidesToShow: 2,
+				slidesToScroll: 1,
 			}
-		 },
-		{
-		  breakpoint: 1100,
-		  settings: {
-			 slidesToShow: 1,
-			 slidesToScroll: 1,
-		  }
 		},
 		{
-		  breakpoint: 920,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			 arrows: false,
-			 TouchMove: true,
-		  }
+			breakpoint: 1100,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 920,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				TouchMove: true,
+			}
 		},
 		{
 			breakpoint: 545,
@@ -42,6 +42,6 @@
 				TouchMove: true,
 				variableWidth: false,
 			}
-		 },
-	 ]
- });
+		},
+	]
+});
